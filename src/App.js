9e941bdb1070e24bbdb1aacc33b454604ff01844
ghvasconcelos <ghvasconcelos@hotmail.com>
2023-08-14 16:23:58 -0300

    v1.2

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
 import './App.css';
-import logo from './image/logo.svg'
+import Logo from './components/nav/index'
+import perfil from './image/perfil.svg'
+import sacola from './image/sacola.svg'
+
+const textoOpcoes = ['CATEGORIAS', 'FAVORITOS', 'MINHA ESTANTE'];
+const icones = [perfil, sacola];
 
 function App() {
   return (
     <div className='App'>
       <header className='App-header'>
-        <div className='logo'>
-          <img src={logo} alt="Logo do site" />
-          <p><strong>Alura</strong>Books</p>
-        </div>
+        <Logo/>
+        <ul className='opcoes'>
+          { textoOpcoes.map( (texto) => (
+            <li className='opcao'><p>{texto}</p></li>
+          ) ) }
+        </ul>   
+        <ul className='icones'>
+        {icones.map( (icone) => (
+            <li className='icone'><img src={icone}></img></li>
+          ))}
+        </ul>     
       </header>
     </div>
-  );
+  );  
 }
 
 export default App;
