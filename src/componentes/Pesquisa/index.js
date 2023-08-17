90df4f729cd6aff1dce23fe8f63433d930ee0840
Gustavo-PC <ghvasconcelos@hotmail.com>
2023-08-17 16:58:10 -0300

    final front

--- src/componentes/Pesquisa/index.js
+++ src/componentes/Pesquisa/index.js
@@ -1,9 +1,7 @@
 import Input from '../Input'
 import styled from 'styled-components'
 import { useState } from 'react'
-import { getLivros } from '../../servicos/livros'
-import { useEffect } from 'react'
-import { postFavorito } from '../../servicos/favoritos'
+import { livros } from './dadosPesquisa'
 
 const PesquisaContainer = styled.section`
     background-image: linear-gradient(90deg, #002F52 35%, #326589 165%);
@@ -49,21 +47,6 @@ const Resultado = styled.div`
 
 function Pesquisa() {
     const [livrosPesquisados, setLivrosPesquisados] = useState([])
-    const [livros, setLivros] = useState([])
-
-    useEffect(() => {
-        fetchLivros()
-    }, [])
-
-    async function fetchLivros() {
-        const livrosDaAPI = await getLivros()
-        setLivros(livrosDaAPI)
-    }
-
-    async function insereFavorito(id) {
-        await postFavorito(id)
-        alert(`O livro de id: ${id} foi inserido com sucesso`)
-    }
 
     return (
         <PesquisaContainer>
@@ -78,7 +61,7 @@ function Pesquisa() {
                 }}
             />
             { livrosPesquisados.map( livro => (
-                <Resultado onClick={() => insereFavorito(livro.id)}>
+                <Resultado>
                     <img src={livro.src}/>
                     <p>{livro.nome}</p>
                 </Resultado>
--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Home from './rotas/Home';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { createGlobalStyle } from 'styled-components'
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Header from './componentes/Header'
-import Favoritos from './rotas/Favoritos';
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -33,13 +30,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <GlobalStyle />
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/favoritos" element={<Favoritos/>} />
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
 
